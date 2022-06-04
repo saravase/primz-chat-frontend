1f274c559d26d1bbdff1d52fa8c5cd000df36738
saravase <saravanakumar323py@gmail.com>
2022-06-04 14:22:50 +0530

    refs #develop Initial commit

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,98 +1,112 @@
 <template>
-  <div class="div_chat_app">
-    <div class="flex flex-row h-screen">
-      <div class="flex flex-col justify-between items-center w-16 bg-gray-200 p-2">
-        <div class="flex flex-col space-y-4">
-          <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-          <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-          <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-          <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-          <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-          <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-        </div>
-        <div class="flex flex-col space-y-4">
-          <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-          <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
-          <a><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+<div class="chat-app">
+  <div class="flex">
+    <div class="w-14 h-screen bg-white flex flex-col items-center justify-between space-y-4">
+      <div class="pt-4 flex-none">
+        <a>
+          <img src="../assets/logo.jpg" class="w-8 h-8 object-cover" />
+        </a>
+      </div>
+      <div class="flex flex-col flex-none">
+        <a class="cursor-pointer p-2   mt-4" @click='changeSelected(0)' >
+          <svg class="w-6 h-6 " fill="none" :stroke="`${selected == 0 ? '#028dde': '#a6a6a6'}`" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+        </a>
+        <a class="cursor-pointer p-2   mt-4" @click='changeSelected(1)' >
+          <svg class="w-6 h-6" fill="none" :stroke="`${selected == 1 ? '#028dde': '#a6a6a6'}`" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+        </a>
+        <a class="cursor-pointer p-2  mt-4" @click='changeSelected(2)'>
+          <svg class="w-6 h-6" fill="none" :stroke="`${selected == 2 ? '#028dde': '#a6a6a6'}`" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"></path></svg>
+        </a>
+        <a class="cursor-pointer p-2  mt-4" @click='changeSelected(3)' >
+          <svg class="w-6 h-6" fill="none" :stroke="`${selected == 3 ? '#028dde': '#a6a6a6'}`" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+        </a>
+        <a class=" cursor-pointer p-2  mt-4" @click='changeSelected(4)' >
+          <svg class="w-6 h-6" fill="none" :stroke="`${selected == 4 ? '#028dde': '#a6a6a6'}`" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>
+        </a>
+      </div>
+      <div class="pb-4 flex-none">
+        <a class="cursor-pointer" @click='changeSelected(5)' >
+          <svg class="w-6 h-6" fill="none" :stroke="`${selected == 5 ? '#028dde': '#a6a6a6'}`" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+        </a>
+      </div>
+    </div>
+    <div class="w-72 h-screen bg-gray-100 flex flex-col px-3 py-3 space-y-3 ">
+      <div class="text-xl text-left">Chats</div>
+      <div class="flex flex-row bg-gray-200 justify-between items-center rounded-md p-1">
+        <input class="text-sm focus:outline-none w-10/12 bg-gray-200 text-gray-500 p-2" type="text" placeholder="Search for messages or users ..." />
+        <div class="w-2/12 flex justify-center items-center">
+          <svg class="w-4 h-4 flex-none cursor-pointer" fill="none" stroke="#a6a6a6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div>
       </div>
-      <div class="w-64 bg-white-200 p-2 flex flex-col flex-none space-y-4">
-        <div class="flex flex-row justify-between items-center mb-4">
-          <h1 class="font-semibold text-2xl">Inbox</h1>
-          <svg class="flex-none w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-        <div class="h-64 bg-red-200"></div>
-        <div class="h-64 bg-red-200"></div>
-      </div>
-      <div class="flex flex-row flex-auto bg-white-200 rounded-tl-xl border-l shadow-xl">
-        <div class="flex flex-col  w-1/5">
-          <div class="flex-none  bg-red-200 h-24">top</div>
-          <div class="flex-auto overflow-y-auto">
-            <a class="block border-b" v-for='index in 10' :key='index'>
-              <div :class="{'border-transparent hover:bg-gray-100' : index != 1}" class=" border-blue-500 border-l-2 p-2 space-y-2">
-                <div class="flex flex-row  items-center space-x-2">
-                  <svg class="flex-none w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                  <strong class="flex-grow mr-1 text-sm text-left">Primz</strong>
-                  <span class="text-sm text-gray-400">3min</span>
-                </div>
-                <div class="flex flex-row items-center space-x-1">
-                  <svg class="flex-none w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                  <div class="flex-grow truncate">Hi optimus, How are you </div>
-                </div>
+      <div class="h-20 min-h-max">
+        <div class=" flex overflow-x-auto space-x-2 bg-slate-100 w-full ">
+          <a class="flex flex-col justify-center items-center p-1 w-16 space-y-1" v-for='(contact, index) in state.contactList' :key='index'>
+            <div class="relative flex-none">
+              <img class="w-8 h-8 object-cover " src="../assets/logo.jpg" />
+              <div class="absolute w-3 h-3 right-0 bottom-0 bg-gray-200 rounded-full flex justify-center items-center">
+                <div class="w-2 h-2 rounded-full" :class="`${contact.online ? 'bg-green-500': ''}`"></div>
               </div>
-            </a>
-          </div>
+            </div>
+            <div class="text-sm flex-grow text-gray-500 truncate w-full flex-auto">{{contact.name}}</div>
+          </a>
         </div>
-        <div class="w-3/5 border-l border-r border-gray-400 flex flex-col">
-          <div class="flex-none h-16 flex flex-row items-center justify-between p-2 border-b">
-            <div class="flex flex-col space-y-1">
-              <strong class="text-left">Kasthoori</strong>
-              <input class="outline-none text-sm placeholder-gray-400 border-b border-dashed border-gray-500" type="text" placeholder="Add conversation title" />
+      </div>
+      <div class="flex flex-col space-y-3 overflow-y-auto h-full mb-3">
+        <div class="flex bg-slate-50 rounded-md p-3 justify-center" v-for='(contact, index) in state.contactList' :key='index'>
+          <div class="w-1/5 flex-none">
+            <img class="w-10 h-10 object-cover rounded-full" src="../assets/logo.jpg" />
+          </div>
+          <div class="w-4/5 flex-grow flex flex-col space-y-1">
+            <div class="flex flex-row justify-between items-center">
+              <div class="text-sm font-bold text-black-500 truncate">{{contact.name}}</div>
+              <div class="text-xs text-gray-300 font-medium flex-none">04:00 PM</div>
             </div>
-            <div class="flex flex-row items-center space-x-4">
-              <svg class="flex-none w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path></svg>
-              <svg class="flex-none w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"></path></svg>
-              <svg class="flex-none w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
-              <svg class="flex-none w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-              <svg class="flex-none w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
-            </div>
-          </div>
-          <div class="flex-none flex-auto overflow-y-auto"
-          style="background-image: url(https://www.whatsappimages.in/wp-content/uploads/2022/03/Beautiful-White-Background-Images-pics-for-download-4.jpg)">
-          <div v-for='index in 20' :key='index'>
-          <div v-if="index%10 == 0" class="flex flex-row justify-center text-sm text-gray-400">You assigned this conversation 5days ago</div>
-          <div class="flex flex-row space-x-2 p-2" :class="{'flex-row-reverse space-x-reverse' :index%2 == 0  }">
-            <svg class="flex-none w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <div class="flex flex-col items-start">
-              <div class="rounded p-5" :class="`${index%2 == 0 ? 'bg-green-200': 'bg-red-200'}`">How are you</div>
-              <div class="text-sm text-gray-400">3hr ago</div>
-            </div>
-          </div>
-          </div>
-          </div>
-          <div class="flex-none h-24 p-2">
-            <textarea class="outline-none border rounded p-2 shadow-lg focus:border-blue-600  hover:border-blue-600 w-full h-full">Message</textarea>
-          </div>
-        </div>
-        <div class="w-1/5 bg-green-100 overflow-y-auto flex flex-col">
-          <div class="flex-none h-64 border-b border-green-400"></div>
-          <div class="flex flex-col space-y-3 p-2">
-            <div class="flex flex-none items-center justify-center h-64 bg-red-200 p-2" v-for='index in 10' :key='index'>
-            Content
-            </div>
+            <div class="text-sm text-gray-500 text-left truncate">Hi, I Love you!! I Love you!! I Love you!!</div>
           </div>
         </div>
       </div>
     </div>
+    <div class="flex-grow h-screen bg-blue-100">
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 
+import { reactive, ref } from 'vue'
+
 export default {
   name: 'ChatView',
   components: {
+  },
+  setup: () => {
+    const selected = ref(2)
 
+    const state = reactive({
+      contactList: [
+        { name: 'kasthoori', online: true },
+        { name: 'saravava', online: false },
+        { name: 'kasthoori', online: true },
+        { name: 'kasthoori', online: true },
+        { name: 'saravava', online: false },
+        { name: 'kasthoori', online: true }
+      ]
+    })
+
+    const changeSelected = (value) => {
+      selected.value = value
+    }
+
+    return {
+      state,
+      selected,
+      changeSelected
+    }
   }
+
 }
 </script>
+
+<style >
+</style>
